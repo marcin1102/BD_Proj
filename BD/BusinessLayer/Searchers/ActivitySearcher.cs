a7d068ea1537a67031383c5ac4373d29a317ea63
Szymon Janota <szymekj18@gmail.com>
2017-07-03 21:24:57 +0200

    add activity searcher

---
+++ BD/BusinessLayer/Searchers/ActivitySearcher.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.DTO;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Searchers
+{
+	class ActivitySearcher
+	{
+		public async Task<IReadOnlyCollection<ActivityData>> GetActivities()
+		{
+			var context = new RepairContext();
+			return context.Activities.Select(activity => new ActivityData
+			{
+				Type = activity.Type,
+				Descr = activity.Descr,
+				Status = activity.Status,
+				Result = activity.Result,
+				ReqId = activity.Request.Id,
+				Request = activity.Request,
+				WorkerId = activity.Worker.Id,
+				Worker = activity.Worker
+			}).ToList();
+		}
+	}
+}
