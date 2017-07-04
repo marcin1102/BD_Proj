7fadd4dd0fd596d4f5904c402947836fba5fc611
Szymon Janota <szymekj18@gmail.com>
2017-07-04 23:58:33 +0200

    add calling activity service create method from view

--- BD/BD/Manager/HandleRequest.cs
+++ BD/BD/Manager/HandleRequest.cs
@@ -7,19 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BD.Helpers;
+using BusinessLayer.DTO;
 
 namespace BD.Manager.beta
 {
     public partial class HandleRequest : UserControl
     {
-        public HandleRequest()
+		private readonly RequestData request;
+
+        public HandleRequest(RequestData request)
         {
             InitializeComponent();
-        }
+			this.request = request;
+		}
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-        }
+			this.GoToNextView(new CreateActivity(this, request));
+		}
     }
 }
