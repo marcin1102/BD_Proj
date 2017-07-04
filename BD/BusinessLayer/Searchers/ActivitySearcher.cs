01e9c4a048a6564c9f3ee05a8f66e09f0f15550c
Szymon Janota <szymekj18@gmail.com>
2017-07-04 23:21:19 +0200

    add activity searching by object id and status

--- BD/BusinessLayer/Searchers/ActivitySearcher.cs
+++ BD/BusinessLayer/Searchers/ActivitySearcher.cs
@@ -27,6 +27,26 @@ namespace BusinessLayer.Searchers
 			}).ToList();
 		}
 
+		public async Task<ICollection<ActivityData>> GetActivities(string status, int objectId)
+		{
+			var context = new RepairContext();
+			var objectEntity = context.Objects.Single(entity => entity.Id == objectId);
+			var requestEntity = context.Requests.Single(entity => entity.Object.Id == objectEntity.Id);
+			return context.Activities
+					.Where(activity => activity.Status == status && activity.Request.Id == requestEntity.Id)
+					.Select(activity => new ActivityData()
+					{
+						Type = activity.Type,
+						Descr = activity.Descr,
+						Status = activity.Status,
+						Result = activity.Result,
+						ReqId = activity.Request.Id,
+						Request = activity.Request,
+						WorkerId = activity.Worker.Id,
+						Worker = activity.Worker
+					}).ToList();
+		}
+
 		public async Task<ICollection<ActivityData>> GetActivities(string status)
 		{
 			var context = new RepairContext();
