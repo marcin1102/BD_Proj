3896cb8f869077d17413263dc4cc41f3abb95477
Marcin Łesek <daav95@gmail.com>
2017-07-04 20:33:08 +0200

    Loginform password textbox updated

--- BD/BD/Login/LoginForm.Designer.cs
+++ BD/BD/Login/LoginForm.Designer.cs
@@ -75,6 +75,7 @@
             // 
             this.passwordTextBox.Location = new System.Drawing.Point(93, 62);
             this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
             this.passwordTextBox.Size = new System.Drawing.Size(77, 20);
             this.passwordTextBox.TabIndex = 4;
             // 
