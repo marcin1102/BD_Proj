1dbd256e413647fcdf75c7dbb8807543370daa81
Szymon Janota <szymekj18@gmail.com>
2017-07-05 18:00:51 +0200

    add select and back button on worker select panel

--- BD/BD/Manager/SelectWorker.Designer.cs
+++ BD/BD/Manager/SelectWorker.Designer.cs
@@ -29,9 +29,11 @@
 		private void InitializeComponent()
 		{
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.searchTextBox = new System.Windows.Forms.TextBox();
 			this.searchButton = new System.Windows.Forms.Button();
+			this.searchTextBox = new System.Windows.Forms.TextBox();
 			this.workerGridView = new System.Windows.Forms.DataGridView();
+			this.selectButton = new System.Windows.Forms.Button();
+			this.backButton = new System.Windows.Forms.Button();
 			this.groupBox1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.workerGridView)).BeginInit();
 			this.SuspendLayout();
@@ -47,13 +49,6 @@
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Wyszukaj pracownika";
 			// 
-			// searchTextBox
-			// 
-			this.searchTextBox.Location = new System.Drawing.Point(6, 18);
-			this.searchTextBox.Name = "searchTextBox";
-			this.searchTextBox.Size = new System.Drawing.Size(453, 20);
-			this.searchTextBox.TabIndex = 1;
-			// 
 			// searchButton
 			// 
 			this.searchButton.Location = new System.Drawing.Point(465, 16);
@@ -64,6 +59,13 @@
 			this.searchButton.UseVisualStyleBackColor = true;
 			this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
 			// 
+			// searchTextBox
+			// 
+			this.searchTextBox.Location = new System.Drawing.Point(6, 18);
+			this.searchTextBox.Name = "searchTextBox";
+			this.searchTextBox.Size = new System.Drawing.Size(453, 20);
+			this.searchTextBox.TabIndex = 1;
+			// 
 			// workerGridView
 			// 
 			this.workerGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -72,10 +74,32 @@
 			this.workerGridView.Size = new System.Drawing.Size(540, 187);
 			this.workerGridView.TabIndex = 2;
 			// 
+			// selectButton
+			// 
+			this.selectButton.Location = new System.Drawing.Point(3, 257);
+			this.selectButton.Name = "selectButton";
+			this.selectButton.Size = new System.Drawing.Size(75, 23);
+			this.selectButton.TabIndex = 3;
+			this.selectButton.Text = "Wybierz";
+			this.selectButton.UseVisualStyleBackColor = true;
+			this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+			// 
+			// backButton
+			// 
+			this.backButton.Location = new System.Drawing.Point(84, 257);
+			this.backButton.Name = "backButton";
+			this.backButton.Size = new System.Drawing.Size(75, 23);
+			this.backButton.TabIndex = 4;
+			this.backButton.Text = "Wróc";
+			this.backButton.UseVisualStyleBackColor = true;
+			this.backButton.Click += new System.EventHandler(this.backButton_Click);
+			// 
 			// SelectWorker
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.backButton);
+			this.Controls.Add(this.selectButton);
 			this.Controls.Add(this.workerGridView);
 			this.Controls.Add(this.groupBox1);
 			this.Name = "SelectWorker";
@@ -92,5 +116,7 @@
 		private System.Windows.Forms.TextBox searchTextBox;
 		private System.Windows.Forms.Button searchButton;
 		private System.Windows.Forms.DataGridView workerGridView;
+		private System.Windows.Forms.Button selectButton;
+		private System.Windows.Forms.Button backButton;
 	}
 }
